29c3069220f29abcf01c53738ccc2f48afd362e3
Zachary StarkJones <zak@newalexandria.org>
2014-08-17 00:19:41 -0400

    correct the loading process for persons

--- flat_file_class/person.rb
+++ flat_file_class/person.rb
@@ -1,6 +1,8 @@
+require 'time'
+load 'person/klass.rb'
+
 class Person
-  require 'person/klass'
-  require 'time'
+  extend Loaders
 
   attr_accessor :last_name, :first_name, :middle_initial,
                 :gender, :birth_date,
@@ -8,7 +10,7 @@ class Person
 
   def initialize opts={}
     opts.each do |k,v|
-      send k.to_sym, v
+      send "#{k}=".to_sym, v
     end  if opts.is_a?(Hash) || opts.map{|e| e.to_a.size}.uniq.size == 1
   end
 
--- flat_file_class/person/klass.rb
+++ flat_file_class/person/klass.rb
@@ -1,33 +1,31 @@
-module Person
-  class << self
-    SOURCES = [
-      {name:'comma_delimited.txt', delim:',',
-       headers:[:first_name, :last_name, :middle_initial, :gender, :favorite_color, :birth_date]},
-      {name:'pipe_delimited.txt',  delim:'|',
-       headers:[:first_name, :last_name, :gender, :favorite_color, :birth_date]},
-      {name:'space_delimited.txt', delim:' ',
-       headers:[:first_name, :last_name, :middle_initial, :gender, :birth_date, :favorite_color]}
-    ]
+module Loaders
+  SOURCES = [
+    {name:'comma_delimited.txt', delim:',',
+     headers:[:first_name, :last_name, :gender, :favorite_color, :birth_date]},
+    {name:'pipe_delimited.txt',  delim:'|',
+     headers:[:first_name, :last_name, :middle_initial, :gender, :favorite_color, :birth_date]},
+    {name:'space_delimited.txt', delim:' ',
+     headers:[:first_name, :last_name, :middle_initial, :gender, :birth_date, :favorite_color]}
+  ]
 
-    def all source_list
-      source_list = SOURCES if source_list.is_a?(Hash) && source_list.any?
-      sources.reduce([]) do |all_records,source_info|
-        all_records += load_source(source_info)
-      end
+  def all source_list=nil
+    sources = SOURCES if !source_list.is_a?(Hash) || source_list.any?
+    sources.reduce([]) do |all_records,source_info|
+      all_records += load_source(source_info)
     end
-
-    def load_source source
-      File.readlines(source[:name]).
-           map{|line| Person.new hash_from(source, line)  }
-    end
-
-  private
-
-    def hash_from source, line
-      Hash[source[:headers].zip(line.split(source[:delim]).map(&:strip))].tap do |h|
-        h[:birth_date] = Date.parse( h[:birth_date] )
-      end
-    end
-
   end
+
+  def load_source source
+    File.readlines("sources/#{source[:name]}").
+         map{|line|  Person.new hash_from(source, line)  }
+  end
+
+private
+
+  def hash_from source, line
+    Hash[source[:headers].zip(line.split(source[:delim]).map(&:strip))].tap do |h|
+      h[:birth_date] = Date.strptime( h[:birth_date].to_s.gsub('-','/'), '%m/%d/%Y' )
+    end
+  end
+
 end
