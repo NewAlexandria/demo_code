2b015a48def17b41bfa5a1f284e6b00d9cd16e2a
ZJ <zak@newalexandria.org>
2019-04-09 21:30:49 -0400

    ttt: account for player spec

--- tictactoe/game.rb
+++ tictactoe/game.rb
@@ -1,12 +1,14 @@
 
 class Game
-  attr_accessor :board, :current_player
+  attr_accessor :board, :active_player
   
   def initialize opts={}
     @board = Game.reset
+    @active_player = 'x'
   end
 
   def move marker, pos
+    raise if !pos.is_a?(Array) && pos.size == 2
   end
 
   def self.reset
