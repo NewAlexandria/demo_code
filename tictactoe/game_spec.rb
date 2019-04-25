30e4948c062af017a8813d6da5a306d8a8c4feb7
ZJ <zak@newalexandria.org>
2019-04-25 12:53:45 -0400

    separate game and spec

---
+++ tictactoe/game_spec.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/sh ruby
+
+describe :game do
+  content 'with one item' do
+    xit 'having even division' do
+    end
+
+    xit 'having uneven division' do
+    end
+  end
+
+  context 'with multiple items' do
+  end
+
+  context 'can initialize' do
+    xit 'with real numbers' do
+    end
+
+    xit 'with integers' do
+    end
+
+    xit 'with notice for only one menu item' do
+    end
+
+  end
+end
