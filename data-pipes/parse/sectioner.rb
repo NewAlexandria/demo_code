1b7f1ed4c4d6369987dae7436d2308182aff5335
ZJ <zak@newalexandria.org>
2019-06-13 23:26:10 -0400

    DP: lint: change indentation levels

--- data-pipes/parse/sectioner.rb
+++ data-pipes/parse/sectioner.rb
@@ -20,50 +20,50 @@ class Sectioner
     pass
   end
 
-    def parse
-      return @sections unless @sections.empty?
+  def parse
+    return @sections unless @sections.empty?
 
-      parser = "\n\n"
-      @sections = @file.split(parser)
-    end
+    parser = "\n\n"
+    @sections = @file.split(parser)
+  end
 
-    def clean
-      @sections.map! do |s|
-        s.split("\n")
-          .map {|line| line.strip }
-          .reject(&:empty?)
-      end
+  def clean
+    @sections.map! do |s|
+      s.split("\n")
+        .map {|line| line.strip }
+        .reject(&:empty?)
     end
+  end
 
-    # TODO cannot handle multiple matching first-line-of-block
-    def annotate
-      file_lines = @file.split("\n")
-      @sections = @sections.reduce([]) do |a, section|
-        line_num = file_lines.index(section.first)
-        a << [section, line_num+1]
-      end
+  # TODO cannot handle multiple matching first-line-of-block
+  def annotate
+    file_lines = @file.split("\n")
+    @sections = @sections.reduce([]) do |a, section|
+      line_num = file_lines.index(section.first)
+      a << [section, line_num+1]
     end
+  end
 
-    # TODO for a superclass method
-    def pass save_target=:disk
-      send save_target
-      puts "saved to #{save_target}"
-    end
+  # TODO for a superclass method
+  def pass save_target=:disk
+    send save_target
+    puts "saved to #{save_target}"
+  end
 
-    def disk
-      CSV.open(filename_suffixed, 'wb') do |csv|
-        csv << @headers
-        @sections.each {|s| csv << s }
-      end
+  def disk
+    CSV.open(filename_suffixed, 'wb') do |csv|
+      csv << @headers
+      @sections.each {|s| csv << s }
     end
+  end
 
-    def filename_suffixed
-      @filename.split(/([\.])/).insert( -3,file_suffix ).join
-    end
+  def filename_suffixed
+    @filename.split(/([\.])/).insert( -3,file_suffix ).join
+  end
 
-    def file_suffix
-      "_#{self.class.name.downcase}"
-    end
+  def file_suffix
+    "_#{self.class.name.downcase}"
+  end
 end
 
 
